c533b75fce86ec5f8d2a703625c6c8f34d1a9172
ryanburge <ryanburge@gmail.com>
2016-04-18 21:40:36 -0500

    Adding Font

--- visuals.R
+++ visuals.R
@@ -8,14 +8,13 @@ literal <- read.csv("D:/literal.csv")
 literal$factor <- c("30s", "50s" , "70s")
 
 
-<<<<<<< HEAD
 g1 <- ggplot(literal, aes(x=m_lit, y=cluster, colour=factor)) + geom_point() + geom_errorbarh(aes(xmin = lo_lit, xmax=hi_lit), height=0)
 
 
 g1 + theme(axis.title.y=element_blank(),
            axis.text.y=element_blank(),
            axis.ticks=element_blank() ) +
-  labs(x="Biblical Literalism") +  theme(legend.title=element_blank()) + geom_vline(xintercept = mean(gss$literal), linetype = "longdash") + theme(legend.position = "bottom")
+  labs(x="Biblical Literalism") +  theme(legend.title=element_blank()) + geom_vline(xintercept = mean(gss$literal), linetype = "longdash") + theme(legend.position = "bottom") +  theme(text=element_text(size=16, family="Georgia"))
 
 CI(test[test$clustname==1,"attnd"])
 CI(test[test$clustname==2,"attnd"])
@@ -27,7 +26,7 @@ g2 <- ggplot(attnd, aes(x=mean, y=cluster, colour=factor)) + geom_point() + geom
 g2 + theme(axis.title.y=element_blank(),
            axis.text.y=element_blank(),
            axis.ticks=element_blank() ) +
-  labs(x="Biblical Literalism") +  theme(legend.title=element_blank()) + geom_vline(xintercept = mean(gss$attnd), linetype = "longdash") + theme(legend.position = "bottom")
+  labs(x="Biblical Literalism") +  theme(legend.title=element_blank()) + geom_vline(xintercept = mean(gss$attnd), linetype = "longdash") + theme(legend.position = "bottom") +  theme(text=element_text(size=16, family="Georgia"))
 
 CI(test[test$clustname==1,"tolerance"])
 CI(test[test$clustname==2,"tolerance"])
@@ -39,15 +38,5 @@ g3 <- ggplot(tolerance, aes(x=mean, y=cluster, colour=factor)) + geom_point() + 
 g3 + theme(axis.title.y=element_blank(),
            axis.text.y=element_blank(),
            axis.ticks=element_blank() ) +
-  labs(x="Tolerance") +  theme(legend.title=element_blank()) + geom_vline(xintercept = mean(gss$tolerance), linetype = "longdash") + theme(legend.position = "bottom")
-=======
-ag <- ggplot(literal, aes(x=m_lit, y=cluster, colour=factor)) + geom_point() + geom_errorbarh(aes(xmin = lo_lit, xmax=hi_lit))
+  labs(x="Tolerance") +  theme(legend.title=element_blank()) + geom_vline(xintercept = mean(gss$tolerance), linetype = "longdash") + theme(legend.position = "bottom") +  theme(text=element_text(size=16, family="Georgia"))
 
-ag <- ggplot(newdata, aes(x=literal, y=cluster, colour=factor)) + geom_point() + geom_errorbarh(aes(xmin = litlower, xmax=lithigher))
-
-
-ag + theme(axis.title.y=element_blank(),
-           +            axis.text.y=element_blank(),
-           +            axis.ticks=element_blank() ) +
-  +     labs(x="Biblical Literalism") +  theme(legend.title=element_blank()) + geom_vline(xintercept = .323, linetype = "longdash")
->>>>>>> be73b91505807ddac73728c93e2b591a39e781ee
